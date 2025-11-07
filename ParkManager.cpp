a554358424841472a2a41995e72eb0073d8f3e3e
alvropz12-iwnl <alvropz12@gmail.com>
2025-11-07 09:21:02 -0600

    Add files via upload

--- ParkManager.cpp
+++ ParkManager.cpp
@@ -1,34 +1,93 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using namespace std;
 
-int bigAvailable = 8;
-int smallAvailable = 20;
-vector<int> parkingB(8,0); //big parking spaces initialized on 0
-vector<int> parkingS(20,0); // small parking spaces initialized on 0
-string carSize;
+typedef struct{
+  vector<bool>g;
+  vector<bool>p;
+} Espacios;
 
-int main(){
-  cout<<"== Welcome to park manager! ==\n";
-  cout<<"select one of the following options: \n 1) Park car \n 2) Exit parking spot \n 3) Check availability \n";
+vector<bool> parkingB(8,0); //big parking spaces initialized on 0
+vector<bool> parkingS(20,0); // small parking spaces initialized on 0
+int carSize;
+int option;
 
-  ;
+vector<Espacios> parkingSpaces(4);
 
+// FUNCTION PROTOTYPE
+void available(void);
+void entrance(void);
+
+int main()
+{
+  for(int i = 0; i < parkingSpaces.size(); i++){
+  parkingSpaces[i].g = vector<bool>(8, false);
+  parkingSpaces[i].p = vector<bool>(20, false);
   }
 
-void available(){
-  if(bigAvailable > 0 || smallAvailable > 0){
-    cout<<"There are "<< bigAvailable <<" big parking spots available & "<<smallAvailable<<"small parking spots available";
-  }
-  else{
-    cout<<"There are no parking spots available in this moment";
-  }
+  bool running = true;
+  while(running == true){
+    cout<<"== Welcome to park manager! ==\n";
+    cout<<"select one of the following options: \n 1) Park your car \n 2) Exit parking spot \n 3) Check availability \n 4) Exit the parking system \n";
+    cout<<"Your option: ";
+    cin >> option;
+
+    switch (option){
+      case 1:
+      break;
+
+      case 2:
+      break;
+
+      case 3:
+      available();
+      break;
+
+      case 4:
+      cout << "Thank you for using our system!";
+      running = false;
+      break;
+
+      default:
+      cout << "Invalid Option" << endl << "Enter a valid option, please" << endl;
+      break;
+    }
+
+    }
 }
 
-void entrance(){
-  cout<<"Is your car big or small? \n"; //en el gui estos serian botones :D
-  cin>>carSize;
-  if(carSize)
-}
+//3) Check availability
+    void available(){
+    for(int i = 0; i < parkingSpaces.size(); i++){
+      int bigAvailable = count(parkingSpaces[i].g.begin(), parkingSpaces[i].g.end(), false);
+      int smallAvailable = count(parkingSpaces[i].p.begin(), parkingSpaces[i].p.end(), false);
+      if(bigAvailable > 0 || smallAvailable > 0){
+      cout<<"There are: "<< bigAvailable <<" big parking spots available &:"<<smallAvailable<<" small parking spots available in level: " << i << endl << endl;
+    }
+    else{
+      cout<<"There are no parking spots available in level: " << i << endl << endl;
+    }
+  }
+
+    }
+
+    //1) Park your car
+    void entrance(){
+    cout<<"Enter 1) Small Car or 2) Big Car"<< endl; //en el gui estos serian botones :D
+    cout<<"Your option: ";
+    cin>>carSize;
+    switch (carSize){
+      case 1:
+      break;
+
+      case 2:
+      break;
+
+      default:
+      cout << "Enter a valid size of Car, Please" << endl;
+      break;
+    }
+  }
